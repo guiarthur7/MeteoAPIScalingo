97ac38b42120b182b43818cd1b847c93676feaa1
guiarthur <arthur.guillaume@ynov.com>
2026-02-18 12:09:17 +0100

    start

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const path = require("path")
+const data = require("./movies.json")
 
 const app = express();
 
@@ -7,10 +8,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
-app.get("/meteo", (req, res) => {
-    res.send("Salut")
+app.get("/api/movies", (req, res) => {
+    res.json(data.movies)
 })
 
-app.listen(8000, () => {
-    console.log("Serveur démarré avec succès")
+app.listen(8080, () => {
+    console.log(`Serveur démarré sur le port 8080`)
 });
